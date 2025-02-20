0f726967449e94cbcfd196ca595b098af9de1a35
petr <petyalom@yandex.ru>
2025-02-20 14:23:56 +0300

    added progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-	<input type="file" @change="onFileChange" />
-	<button @click="uploadFile">upload</button>
+	<input type="file" @change="uploadFile" />
 	<p v-if="uploadStatus">{{ uploadStatus }}</p>
-	<a v-if="key.length > 0" :href="getUrl">Download</a>
-	<button v-if="key.length > 0" @click="copyToClipboard">Copy</button>
+	<progress v-if="uploadProgress > 0" :value="uploadProgress" max="100"/>
+	<p v-if="errorMessage" style="color: red">{{ errorMessage }}</p>
+	<button v-if="key.length > 0" @click="copyToClipboard">Copy to clipboard</button>
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
 	name: 'App',
 
@@ -16,54 +14,59 @@ export default {
 		selectedFile: null,
 		uploadStatus: '',
 		key: '',
+		uploadProgress: 0,
+		errorMessage: "",
 	}),
 
 	methods: {
-		onFileChange(event) {
-			this.selectedFile = event.target.files[0];
-		},
-
-		async uploadFile() {
-			if (!this.selectedFile) {
-				this.uploadStatus = 'Select file before uploading';
-				return;
-			}
-
-			const formData = new FormData();
-			formData.append('file', this.selectedFile);
-
+		uploadFile(event) {
+			this.errorMessage = ""
+			const file = event.target.files[0]
+			if (!file) return
+			const formData = new FormData()
+			formData.append("file", file)
+			const xhr = new XMLHttpRequest()
+			xhr.open("POST", "/upload", true)
+			xhr.upload.addEventListener("progress", (event) => {
+				if (event.lengthComputable) {
+					this.uploadProgress = Math.round((event.loaded / event.total) * 100)
+				}
+			})
+			xhr.addEventListener("load", () => {
+				if (xhr.status === 200) {
+					this.key = JSON.parse(xhr.responseText).key
+					this.uploadProgress = 100
+				} else {
+					this.errorMessage = `Upload failed with status: ${xhr.status} - ${xhr.statusText}`
+				}
+			})
+			xhr.addEventListener("error", () => {
+				this.errorMessage = "Error uploading file. Please check server logs."
+			})
+			xhr.addEventListener("abort", () => {
+				this.errorMessage = "Upload aborted."
+			})
 			try {
-				await axios.post('/upload', formData, {
-					headers: {
-						'Content-Type': 'multipart/form-data'
-					}
-				}).then((response) => {
-					this.key =  response.data.key;
-				});
+				xhr.send(formData)
 			} catch (error) {
-				console.error('File upload error:', error);
-				this.uploadStatus = 'File uploading error';
+				this.errorMessage = `Exception occurred: ${error.message}`
+				console.error(this.errorMessage)
 			}
 		},
 
 		copyToClipboard() {
 			if (!this.selectedFile) {
-				this.uploadStatus = 'There is no file to copy';
-				return;
+				this.uploadStatus = 'There is no file to copy'
+				return
 			}
 			navigator.clipboard.writeText(`${window.location.protocol}//${window.location.host}/${this.key}`)
-				.catch(err => {
-					console.error('Error copying to clipboard:', err);
-					this.uploadStatus = 'Error while copying';
-				});
+					.catch(err => {
+						console.error('Error copying to clipboard:', err)
+						this.uploadStatus = 'Error while copying'
+					})
 		}
 	},
 
-	computed: {
-		getUrl() {
-			return `${window.location.protocol}${this.key}`
-		}
-	}
 }
 </script>
 
